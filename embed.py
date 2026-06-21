9f76fa2b57d38a83595618136d55610790feae70
Sathish-Kumar0123 <153310475+Sathish-Kumar0123@users.noreply.github.com>
2026-06-21 11:39:16 +0530

    Update embed.py

--- embed.py
+++ embed.py
@@ -1,5 +1,5 @@
 import os
-from langchain.schema import Document
+from langchain_core.documents import Document
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
